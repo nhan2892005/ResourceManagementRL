c959a93993eb6fde71c12936409c45787e941e18
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2025-01-06 09:24:01 +0700

    update: Tensorflow adaptive

--- RLBrain_Tensorflow/Policy_gradient.py
+++ RLBrain_Tensorflow/Policy_gradient.py
@@ -1,10 +1,11 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.keras import layers, optimizers
 
 # reproducible
 np.random.seed(1)
 tf.random.set_seed(1)
+
 
 class PolicyGradient:
     def __init__(
@@ -24,64 +25,47 @@ class PolicyGradient:
         self._build_net()
 
         if output_graph:
-            # $ tensorboard --logdir=logs
-            # http://0.0.0.0:6006/
-            # tf.train.SummaryWriter soon be deprecated, use following
-            self.summary_writer = tf.summary.create_file_writer("logs/")
+            # TensorBoard logging
+            self.log_dir = "logs/"
+            self.summary_writer = tf.summary.create_file_writer(self.log_dir)
 
     def _build_net(self):
-        inputs = keras.layers.Input(shape=(self.n_features,), name='observations')
-        
-        layer = keras.layers.Dense(
+        # Define inputs using tf.keras.Input
+        inputs = tf.keras.Input(shape=(self.n_features,), name="observations")
+        self.tf_obs = inputs
+
+        # Define hidden layer (fc1)
+        layer = layers.Dense(
             units=10,
-            activation='tanh',
-            kernel_initializer=keras.initializers.RandomNormal(mean=0, stddev=0.3),
-            bias_initializer=keras.initializers.Constant(0.1),  # Fixed bias initializer
-            name='fc1'
+            activation=tf.nn.tanh,
+            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+            bias_initializer=tf.constant_initializer(0.1),
         )(inputs)
 
-        all_act = keras.layers.Dense(
+        # Define output layer (fc2)
+        all_act = layers.Dense(
             units=self.n_actions,
             activation=None,
-            kernel_initializer=keras.initializers.RandomNormal(mean=0, stddev=0.3),
-            bias_initializer=keras.initializers.Constant(0.1),  # Fixed bias initializer
-            name='fc2'
+            kernel_initializer=tf.random_normal_initializer(mean=0, stddev=0.3),
+            bias_initializer=tf.constant_initializer(0.1),
         )(layer)
 
-        self.all_act_prob = keras.layers.Softmax(name='act_prob')(all_act)
-        self.model = keras.Model(inputs=inputs, outputs=self.all_act_prob)
-        self.optimizer = keras.optimizers.Adam(learning_rate=self.lr)
+        # Use softmax to calculate probabilities
+        all_act_prob = layers.Softmax(name='act_prob')(all_act)
+        self.all_act_prob = all_act_prob
 
-    @tf.function
-    def _train_step(self, observations, actions, advantages):
-        # Reshape observations if needed
-        observations = observations.reshape(-1, self.n_features)
-        
-        with tf.GradientTape() as tape:
-            logits = self.model(observations, training=True)
-            neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                logits=logits, labels=actions)
-            loss = tf.reduce_mean(neg_log_prob * advantages)
-            
-        grads = tape.gradient(loss, self.model.trainable_variables)
-        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-        return loss
-    
+        # Define the model
+        self.model = tf.keras.Model(inputs=inputs, outputs=all_act_prob)
+
+        # Optimizer
+        self.optimizer = optimizers.Adam(learning_rate=self.lr)
+
+
     def choose_action(self, observation):
-        # Reshape observation to match expected input shape (None, n_features)
-        observation = observation.reshape(-1, self.n_features)
-        
-        # Get action probabilities from model
-        prob_weights = self.model(observation, training=False)
-        
-        # Choose action based on probabilities
-        action = np.random.choice(range(self.n_actions), p=prob_weights.numpy().ravel())
+        #observation = np.expand_dims(observation, axis=0)  # Add batch dimension
+        prob_weights = self.model.predict(observation)
+        action = np.random.choice(range(prob_weights.shape[1]), p=prob_weights.ravel())  # Select action based on probabilities
         return action
-    
-    def store_transition(self, s, a, r):
-        self.ep_obs.append(s)
-        self.ep_as.append(a)
-        self.ep_rs.append(r)
 
     def store_ob(self, s):
         self.ep_obs.append(s)
@@ -92,31 +76,43 @@ class PolicyGradient:
     def store_adv(self, r):
         self.ep_rs.append(r)
 
-    def learn(self):
-        discounted_ep_rs = self._discount_and_norm_rewards()
-        loss = self._train_step(
-            tf.convert_to_tensor(np.vstack(self.ep_obs), dtype=tf.float32),
-            tf.convert_to_tensor(np.array(self.ep_as), dtype=tf.int32),
-            tf.convert_to_tensor(discounted_ep_rs, dtype=tf.float32)
-        )
+    def learn(self, all_ob, all_action, all_adv):
+        # Discount and normalize rewards
+        discounted_ep_rs_norm = self._discount_and_norm_rewards()
+
+        # Convert episode data to arrays
+        # all_ob = np.vstack(self.ep_obs)
+        # all_action = np.array(self.ep_as)
+        # all_adv = discounted_ep_rs_norm
+
+        with tf.GradientTape() as tape:
+            logits = self.model(all_ob, training=True)
+            neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=all_action)
+            loss = tf.reduce_mean(neg_log_prob * all_adv)  # Reward-guided loss
+
+        # Compute gradients and apply them
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        # Clear episode data
         self.ep_obs, self.ep_as, self.ep_rs = [], [], []
-        return loss
+        return loss.numpy()
 
     def _discount_and_norm_rewards(self):
+        # Compute discounted rewards
         discounted_ep_rs = np.zeros_like(self.ep_rs)
         running_add = 0
-        for t in reversed(range(0, len(self.ep_rs))):
+        for t in reversed(range(len(self.ep_rs))):
             running_add = running_add * self.gamma + self.ep_rs[t]
             discounted_ep_rs[t] = running_add
+
+        # Normalize rewards
         discounted_ep_rs -= np.mean(discounted_ep_rs)
         discounted_ep_rs /= np.std(discounted_ep_rs)
         return discounted_ep_rs
 
-    def save_data(self, pg_resume):
-        self.model.save_weights(pg_resume + '.h5')
+    def save_data(self, path):
+        self.model.save_weights(path)
 
-    def load_data(self, pg_resume):
-        try:
-            self.model.load_weights(pg_resume + '.h5')
-        except:
-            print(f"Could not load weights from {pg_resume}. Starting with fresh model.")
+    def load_data(self, path):
+        self.model.load_weights(path)
